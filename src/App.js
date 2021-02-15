28c9e63a9dbc0364fb66dee56537a8b0bc09405a
Jaafar Mohammed <jmohsa@gmail.com>
2021-02-15 13:50:20 +0300

    modified files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from './Login'
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+  const [token, setToken] = useState(null);
+  //run code basded on a given condition runs once if input is emoty and chn ages once input changes 
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token)
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log('👨', user);
+      });
+    }
+    console.log("I HAVE A TOKEN 👉", token);
+  }, [])
+
+
   return (
     //bem convetion
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <Player />) :
+          <Login />
+      }
+
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -1,3 +1,31 @@
 //https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
 
-export const authEndpoint = "https://accopunts.spotify.com/authorize";
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "6f612d942fea487d9544c0d971d395c9";
+const redirectUri = "http://localhost:3000/";
+
+
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+//get token from the url;
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substr(1)
+        .split('&')
+        .reduce((initial, item) => {
+            //#accessToken= my 
+            let parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            return initial;
+        }, {});
+}
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scopes=${scopes.join("%20")}&response_type=token&show_dialog=true`;
